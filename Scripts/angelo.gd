e5795dcbfb96be8389c902f89dbd0b2da4bf82b8
hcthoemmes <32181950+hcthoemmes@users.noreply.github.com>
2025-08-16 03:41:56 -0700

    Fishing in 3D space

--- Scripts/angelo.gd
+++ Scripts/angelo.gd
@@ -1,26 +1,39 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+# There's some stuff I added that could/should be handled in a singleton (fishing state, scene loading)
+# I'll fix it later ÷P
+var fishing_possible := false
+var is_fishing		 := false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if not is_on_floor(): velocity += get_gravity() * delta
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("west", "east", "north", "south")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-		# Little hack, not actually what we wanna do cause it won't work for interaction and stuff but fine for a test :]
-		$joint0.rotation.y = atan2(velocity.x,velocity.z) + deg_to_rad(135)
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	# If outside fishing minigame, get input
+	if !is_fishing:
+
+		if fishing_possible:
+			if Input.is_action_just_pressed("use"):
+				is_fishing = true
+				var f = load("res://Scenes/fishing.tscn").instantiate()
+				get_node("../CanvasLayer").add_child(f)
+			
+		var input_dir := Input.get_vector("west", "east", "north", "south")
+		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+			# Little hack, not actually what we wanna do cause it won't work for interaction and stuff but fine for a test :]
+			$joint0.rotation.y = atan2(velocity.x,velocity.z) + deg_to_rad(135)
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_fishin_hole_body_entered(body: Node3D) -> void: fishing_possible = true
+func _on_fishin_hole_body_exited(body: Node3D) -> void: fishing_possible = false
